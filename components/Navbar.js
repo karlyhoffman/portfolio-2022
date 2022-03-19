5ba3540620b4da27ebf65dc3aace6e040503fa64
Karly Hoffman <karlyhoffman@gmail.com>
2022-03-19 15:51:41 -0700

    navbar — add smooth scroll on item click

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,20 @@
 import { useContext } from "react";
 import classNames from "classnames";
+import { gsap } from "gsap";
+import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
 import CurrentSectionContext from "context/CurrentSection";
 import styles from "styles/components/navbar.module.scss";
+
+gsap.registerPlugin(ScrollToPlugin);
 
 const SECTIONS = ["home", "about", "skills", "work", "contact"];
 
 function Navbar() {
   const { section } = useContext(CurrentSectionContext);
+
+  const handleButtonClick = ({ scrollTo }) => {
+    gsap.to(window, { duration: 1, scrollTo });
+  };
 
   return (
     <header className={styles.navigation}>
@@ -17,7 +25,12 @@ function Navbar() {
               key={id}
               className={classNames({ [styles.active]: section === id })}
             >
-              <a href={`#${id}`}>{id}</a>
+              <button
+                onClick={() => handleButtonClick({ scrollTo: `#${id}` })}
+                type="button"
+              >
+                {id}
+              </button>
             </li>
           ))}
         </ul>
