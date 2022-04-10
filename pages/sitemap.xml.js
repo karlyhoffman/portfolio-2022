38d2353a27f1e82f8efe191cb7c0b5d0514f90b9
Karly Hoffman <karlyhoffman@gmail.com>
2022-04-10 14:54:14 -0500

    Create a sitemap.xml

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,34 @@
+const Sitemap = () => null;
+
+const PAGES = ['/'];
+
+const createSitemap = ({ data = [], url }) =>
+  `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${data
+        .map(
+          (loc) =>
+            `<url>
+              <loc>${url + loc}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>`
+        )
+        .join('')}
+    </urlset>
+  `;
+
+export default Sitemap;
+
+export const getServerSideProps = async ({ req, res }) => {
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(
+    createSitemap({
+      data: PAGES,
+      url: req.headers?.host || 'https://karlyhoffman.com',
+    })
+  );
+  res.end();
+  return { props: {} };
+};
