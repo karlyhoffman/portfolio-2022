8811dcd5102b0d3cfdd58732abc05bbe9ea32285
Karly Hoffman <karlyhoffman@gmail.com>
2023-01-28 17:25:36 -0600

    analytics update

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -1,25 +1,33 @@
-const GoogleAnalytics = () => (
-  <>
-    <script
-      async
-      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-    />
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
+const isProduction = process.env.NODE_ENV === 'production';
+
+const GoogleAnalytics = () => {
+  console.log('GA', { isProduction });
+
+  if (!isProduction) return null;
+
+  return (
+    <>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`,
-      }}
-    />
-  </>
-);
+        }}
+      />
+    </>
+  );
+};
 
 export const GoogleAnalyticsEvent = ({ action, params }) => {
-  if (window?.gtag && action && params) {
+  if (isProduction && window?.gtag && action && params) {
     window.gtag('event', action, params);
-  };
+  }
 };
 
 export default GoogleAnalytics;
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
-import { CurrentSectionProvider } from "context/CurrentSection";
-import { Layout } from "components";
-import "styles/global.scss";
+import { Analytics } from '@vercel/analytics/react';
+import { CurrentSectionProvider } from 'context/CurrentSection';
+import { Layout } from 'components';
+import 'styles/global.scss';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 function MyApp({ Component, pageProps }) {
+  console.log({ isProduction });
+
   return (
-    <CurrentSectionProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </CurrentSectionProvider>
+    <>
+      <CurrentSectionProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CurrentSectionProvider>
+      {isProduction && <Analytics />}
+    </>
   );
 }
 
