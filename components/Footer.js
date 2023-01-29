dcd4889602cfce42257318c80a5b7e37deaf0eac
Karly Hoffman <karlyhoffman@gmail.com>
2023-01-28 18:45:15 -0600

    footer — copy update

--- components/Footer.js
+++ components/Footer.js
@@ -5,14 +5,18 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.col_left}>
-        Built with{' '}
+        Built with{" "}
         <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
           <strong>Next.js</strong>
-        </a>
-        {' '}in March 2022
+        </a>{" "}
+        March 2022
       </div>
       <div className={styles.col_right}>
-        <a href="https://github.com/karlyhoffman/portfolio-2022" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/karlyhoffman/portfolio-2022"
+          target="_blank"
+          rel="noreferrer"
+        >
           View Source Code
         </a>
       </div>
